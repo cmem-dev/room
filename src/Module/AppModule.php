fd70b7f34ccb5bcc57ea6088c6aaf94999069063
cmem-dev <cmem@noreal.com>
2016-03-22 23:09:32 +0900

    アプリケーションにmonologインストール

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -6,6 +6,8 @@ use BEAR\Package\PackageModule;
 use Ray\Di\AbstractModule;
 use josegonzalez\Dotenv\Loader as Dotenv;
 use BEAR\Package\Provide\Router\AuraRouterModule;
+use Psr\Log\LoggerInterface;
+use Ray\Di\Scope;
 
 class AppModule extends AbstractModule
 {
@@ -20,5 +22,6 @@ class AppModule extends AbstractModule
         ]);
         $this->install(new PackageModule);
         $this->override(new AuraRouterModule);
+        $this->bind(LoggerInterface::class)->toProvider(MonologLoggerProvider::class)->in(Scope::SINGLETON);
     }
 }

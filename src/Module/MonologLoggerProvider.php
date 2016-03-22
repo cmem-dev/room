b537d4ee7743497ab8efea562cece709607bf761
cmem-dev <cmem@noreal.com>
2016-03-22 23:08:57 +0900

    monologプロバイダ追加

---
+++ src/Module/MonologLoggerProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Cmem\Room\Module;
+
+use BEAR\AppMeta\AbstractAppMeta;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Ray\Di\ProviderInterface;
+
+class MonologLoggerProvider implements ProviderInterface
+{
+    /**
+     * @var AbstractAppMeta
+     */
+    private $appMeta;
+
+    public function __construct(AbstractAppMeta $appMeta)
+    {
+        $this->appMeta = $appMeta;
+    }
+
+    public function get()
+    {
+        $log = new Logger('weekday');
+        $log->pushHandler(
+            new StreamHandler($this->appMeta->logDir . '/weekday.log')
+        );
+
+        return $log;
+    }
+}
